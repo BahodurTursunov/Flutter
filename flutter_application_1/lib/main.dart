c63bb4e2c04a0fcbe93169101b6f613a89ea3711
Ваше Имя <you@example.com>
2022-08-08 08:37:53 +0300

    first app

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          title: Text("My first App"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text(
+            "This is my homepage",
+            style: TextStyle(
+                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
+      ),
+    ),
+  );
+}
